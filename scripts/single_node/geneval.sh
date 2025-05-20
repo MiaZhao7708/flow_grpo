5c456912471e4befe432da7755ee874037a6ac13
MiaZhao7708 <mia.zhaoyq@gmail.com>
2025-05-20 12:52:44 +0000

    debug_sh

--- scripts/single_node/geneval.sh
+++ scripts/single_node/geneval.sh
@@ -1,5 +1,3 @@
-# 1 GPU
-# conda activate flow_grpo
-accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:geneval_sd3_debug
+#! /bin/bash
 # # 4 GPU
-# accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=4 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:geneval_sd3
+accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=4 --main_process_port 29503 scripts/train_sd3.py --config config/dgx.py:geneval_sd3
